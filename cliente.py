63597ebf2d4b6fd84e101ceb72157f9d646880aa
Hugo Toledo <hugo.toledo@dinagram.es>
2025-07-28 13:46:45 +0200

    Agregar cliente.py para consumir la clave desde la API

--- cliente.py
+++ cliente.py
@@ -1,12 +1,12 @@
 import requests
 
 URL = "https://proyecto-seguro-wdnu.onrender.com/api/clave"
-PARAMS = {"clave": "####"}  # misma clave que guardaste en Render
+PARAMS = {"clave": "clave-desde-render"}
 
 response = requests.get(URL, params=PARAMS)
 
 if response.status_code == 200:
     data = response.json()
-    print("✅ Clave recibida:", data["clave_de_acceso"])
+    print("🔐 Clave obtenida desde la API:", data["clave_de_acceso"])
 else:
-    print("❌ Error:", response.json()["error"])
+    print("❌ Error al obtener la clave:", response.status_code)
